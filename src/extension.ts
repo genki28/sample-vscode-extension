5b58f75f1066d6d2202c76ef8c81b20f20127012
genki28 <taiiku39@gmail.com>
2022-03-18 03:29:32 +0900

    お遊び

--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,26 @@
 import * as vscode from 'vscode'
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log(
-    'Congratulations, your extension "smaple-vscode-extension" is now active!'
-  )
-
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
-    'smaple-vscode-extension.helloWorld',
+    'smaple-vscode-extension.convertAllTextToSample',
     () => {
-      vscode.window
-        .showInputBox({
-          prompt: 'Tiny Calculator: ',
-          validateInput: (param) => {
-            const regex = /\d+\s*[-+*/]\s*\d/
-            return regex.test(param) ? '' : 'input: number [-+*/] number'
-          },
-        })
-        .then((value) => {
-          if (value === undefined) {
-            return
-          }
-          const result = eval(value)
-          vscode.window.showInformationMessage(value + '=' + result)
-        })
+      console.log('sampleEditor')
+
+      const editor = vscode.window.activeTextEditor
+      const doc = editor?.document
+      let currentSelection = editor?.selection
+
+      if (editor?.selection.isEmpty && doc) {
+        const startPos = new vscode.Position(0, 0)
+        const endPos = new vscode.Position(doc.lineCount - 1, 100000)
+        currentSelection = new vscode.Selection(startPos, endPos)
+      }
+      editor?.edit((edit) => {
+        edit.replace(currentSelection!, 'sample')
+      })
     }
   )
 
